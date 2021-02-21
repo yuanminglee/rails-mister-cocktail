a4c0306ad8c6de2e822699c54110f02527e091fb
YuanMing Lee <yuanming.lee91@gmail.com>
2021-02-22 01:52:50 +0800

    Seed 100 ingredients

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.12.0'
 gem 'simple_form'
+gem 'httparty'
+gem 'pry-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+response = HTTParty.get('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+
+response['drinks'].map(&:values).flatten.each do |ingredient|
+  Ingredient.create!(name: ingredient)
+end
