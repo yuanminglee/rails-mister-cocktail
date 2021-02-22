7d8fb5d0acc02416fd30e8da194aabc881a7bc07
YuanMing Lee <yuanming.lee91@gmail.com>
2021-02-22 21:40:17 +0800

    Implement features

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'cocktails#index'
+
+  resources :cocktails do
+    resources :doses, only: %i[new create]
+  end
+
+  resources :doses, only: :destroy
 end
